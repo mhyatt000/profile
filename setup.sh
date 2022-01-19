1c9c20d88d7ab081b4436b72efdb4619988e82eb
Matthew Hyatt <mhyatt000@gmail.com>
2022-01-19 16:07:43 -0600

    added python packages

--- setup.sh
+++ setup.sh
@@ -1,6 +1,18 @@
 # set up basic env
 
 # if OSX then install homebrew
+sudo apt-get install curl
+
+## get homebrew
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 sudo apt-get install git;
 sudo apt-get install tree;
+sudo apt-get install python3;
+sudo apt-get install xdotool;
+sudo apt-get install ffmpeg;
+
+# PYTHON PACKAGES
+pip3 install black;
+pip3 install isort;
+pip3 install youtube_dl;
