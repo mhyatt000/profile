30245917ec8d9bec757dab5ffb13cf62f40ae224
Matthew Hyatt <mhyatt000@gmail.com>
2023-09-28 17:43:59 -0500

    deploy

---
+++ rpi-setup.sh
@@ -0,0 +1,10 @@
+
+sudo apt update ; sudo apt upgrade
+sudo apt install openssh-server vim git 
+
+
+# for ROS
+sudo apt install -y build-essential cmake git libbullet-dev python3-colcon-common-extensions python3-pip python3-vcstool wget
+
+
+
