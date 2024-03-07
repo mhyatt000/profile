47d9db2bd676c5469c048a85425646342320f396
Matthew Hyatt <mhyatt000@gmail.com>
2024-03-06 19:21:10 -0600

    deploy

--- profile/install/vncserver.sh
+++ profile/install/vncserver.sh
@@ -1,4 +1,9 @@
 sudo apt-get update
+sudo apt-get install xserver-xorg-video-dummy
 sudo apt-get install -y xfce4 xfce4-goodies
 sudo apt-get install -y tightvncserver
+
+# make sure there is no firewall blocking the vncserver
+sudo ufw allow 5901 5900
+
 vncserver
