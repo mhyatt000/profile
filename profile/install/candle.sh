b1e6a91ced9bb561cae742e1fb86158173989c83
Matthew Hyatt <mhyatt000@gmail.com>
2024-03-06 18:01:22 -0600

    deploy

--- profile/install/candle.sh
+++ profile/install/candle.sh
@@ -1,11 +1,12 @@
 sudo apt-get install -y  build-essential
-sudo apt-get install -y qt5-default qt5-qmake libqt5serialport5-dev make gcc g++ git
+sudo apt-get install -y qt5-default qtbase5-dev qt5-qmake  libqt5serialport5-dev make gcc g++ git
+
 
 cd ~/Downloads
 
 git clone https://github.com/Denvi/Candle.git
 
-cd Candle/src
+cd ~/Downloads/Candle/src
 
 qmake
 make
